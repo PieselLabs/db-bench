00304396556cb4fb2e948fb190e5f98e0b0f80d1
Artem Alekseev <fexolm@gmail.com>
2024-01-24 10:44:05 +0300

    Add arrow

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-fn main() {
-    use std::fs::File;
+use parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder;
+use std::fs::File;
 
-    fn main() -> anyhow::Result<()> {
-        let file = File::open("example.csv")?;
-        println!("Hello, world!");
-        Ok(())
-    }
+fn main() -> anyhow::Result<()> {
+    let file = File::open("example.parquet")?;
 
+    let builder = ParquetRecordBatchReaderBuilder::try_new(file).unwrap();
+
+    println!("Converted arrow schema is: {}", builder.schema());
+
+    let mut reader = builder.build().unwrap();
+    let record_batch = reader.next().unwrap().unwrap();
+
+    println!("Read {} records.", record_batch.num_rows());
+    Ok(())
 }
